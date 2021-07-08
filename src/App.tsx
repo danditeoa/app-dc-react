ad7148233d23ddf41bcada844a16fcbc0ffe365e
danditeoa <taci.dandi@gmail.com>
2021-07-07 21:05:36 -0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 import Orders from "./components/Orders/Orders";
 import { OrderDetails } from "./components/Orders/components/OrderDetails/OrderDetails";
 import Home from "./components/Home/Home";
 import "./App.scss";
 
+
 export default function App() {
   return (
     <Router>
-      <div className="app">
+      <div>
         <nav>
+            dfsdfds
           <ul>
+
             <li>
               <Link to="/">Home</Link>
             </li>
@@ -22,23 +30,13 @@ export default function App() {
             </li>
           </ul>
         </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
+        <div className="app">
+          <Switch>
             <Route path="/" component={Home} exact />
             <Route path="/order-details" component={OrderDetails} />
             <Route path="/orders" component={Orders} />
-          {/* <Route path="/order-detais">
-            <OrderDetails />
-          </Route>
-          <Route path="/orders">
-            <Orders />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route> */}
-        </Switch>
+          </Switch>
+        </div>
       </div>
     </Router>
   );
--- src/components/Orders/components/OrderDetails/OrderDetails.tsx
+++ src/components/Orders/components/OrderDetails/OrderDetails.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import {RouteComponentProps} from 'react-router'
+import BackButton from '../../../BackButton/BackButton';
 import "./order-details.scss";
 
 export const OrderDetails = ({location}: RouteComponentProps<{}, {}>) => {
   const data = location.state;
+  console.log('DATA', data)
 
   return (
-    <div>
-      {JSON.stringify(data) }
+    <div className="container">
+      <BackButton />
+     {data.amount}
     </div>
   )
 }
